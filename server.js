21105ec1f444e76f5a3000b9ee76e05c31a35f58
owengombas <owen.gombas@gmail.com>
2017-06-02 18:59:37 +0200

    Update

--- server.js
+++ server.js
@@ -3,27 +3,31 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var gpio = require('pi-gpio');
-server.listen(3000);
+server.listen(3000);     // THE SERVER PORT
 app.use('/', express.static(__dirname + '/client'));
 
-var pins = [7, 11, 15];
+var pins = [7, 11, 15]; // PUT YOUR LEDs PINS HERE
 
 io.on('connection', function (client) {
-     console.log('connected');
+
+     // WHEN THE CLIENT EMIT "TurnOn"
      client.on('TurnOn', function(pin) {
           TurnOn(pin);
      });
 
+     // WHEN THE CLIENT EMIT "TurnOff"
      client.on('TurnOff', function(pin) {
           TurnOff(pin);
      });
 
+     // WHEN A CLIENT DISCONNECT
      client.on('disconnect', function() {
           pins.forEach(function(e){
                TurnOff(e);
           })
      });
 
+     // TO TURN OFF A LED (pin -> pin were your led is plugged)
      function TurnOn(pin) {
           console.log(pin + " 1");
           gpio.open(pin, "output", function(err) {
@@ -31,6 +35,7 @@ io.on('connection', function (client) {
           });
      }
 
+     // TO TURN OFF A LED (pin -> pin were your led is plugged)
      function TurnOff(pin) {
           console.log(pin + " 0");
           gpio.open(pin, "output", function(err) {
